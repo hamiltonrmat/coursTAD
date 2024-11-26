f73fad3b35eed913fe5f893ad981e92aadbebacd
Hamilton Araujo <117686961+hamiltonrmat@users.noreply.github.com>
2024-11-26 18:22:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
 import streamlit as st
+import pandas as pd
+from sklearn.datasets import load_wine
 
-st.write("Hello Worldsss")
+values, target = load_wine(return_X_y=True)
+target_names = load_wine().target_names
+feature_names = load_wine().feature_names
+
+st.title('Bonjour !')
+
+df = pd.DataFrame(values, columns=feature_names)
+
+
+
+st.line_chart(df[''])
+df
